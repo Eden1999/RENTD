6b4de95d30a458e222437784267fd2a2af9ca16e
shirelronis <shirelbar100@gmail.com>
2022-03-20 17:10:18 +0200

    showing profile on hosts profile

--- client/src/components/UserManagment/Profile.js
+++ client/src/components/UserManagment/Profile.js
@@ -1,6 +1,12 @@
 
 import React, { useState, useCallback, useContext, useEffect } from "react"
 import { AppContext } from "../../store/AppContext";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 import PropTypes from 'prop-types';
 import axios from 'axios'
 
@@ -10,27 +16,6 @@ const Profile = () => {
     // name, location_x, location_y, cost_per_hour, capacity, wifi, disabled_access, smoke_friendly, description, space_type_id
     const [{ user }] = useContext(AppContext);
     const [workspaces, setWorkspaces] = useState([])
-
-    // const getUserData = async () => {
-    //     const tokenId = token
-    //     await axios.get(`http://localhost:8000/users/${tokenId}`, {
-    //         headers: {
-    //             token
-    //         }
-    //     })
-    //     .then((res) => {
-    //         console.log("success")
-    //         setUser(res.data)
-    //     })
-    //     .catch(err =>{
-    //         alert(err.response.data)
-    //         setUser({})
-    //     })
-    // }
-
-    // useEffect(() => {
-    //     getUserData()
-    // }, [])
 
     const getHostWorkspaces = async () => {
             await axios.get(`http://localhost:8000/workspaces/hosts/${user.id}`, {
@@ -52,16 +37,33 @@ const Profile = () => {
     return (
         <div>
             <div className="profile-data">
-            Profile
-            <img src={user.profileIcon || "emptyProfileIcon.jpeg"}/>
-            <a>name: {user.username}</a>
-            <br/>
-            <a>email: {user.email}</a>
-            <br/>
-            <a>password: {user.password}</a>
+                Profile
+                <img src={user.profileIcon || "emptyProfileIcon.jpeg"}/>
+                <a>name: {user.username}</a>
+                <br/>
+                <a>email: {user.email}</a>
+                <br/>
+                <a>password: {user.password}</a>
             </div>
             <div className="host-places">
-                <a></a>
+                <a>My Workspaces</a>
+                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                    {workspaces && workspaces.map(workspace => 
+                        <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar alt={workspace.name} src={workspace.icon || "/emptyProfileIcon.jpeg"} />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={workspace.name}
+                            secondary={
+                                <React.Fragment>
+                                    {workspace.description}
+                                </React.Fragment>
+                            }
+                        />
+                    </ListItem>
+                    )}
+                </List>
             </div>
         </div>
     )
