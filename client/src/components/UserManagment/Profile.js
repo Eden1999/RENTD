b108b174befdf41d9741a88be143abd6fc794e33
amit ram <amit0123ram@gmail.com>
2022-03-28 16:48:47 +0300

    feat(profile): add profile image edit

--- client/src/components/UserManagment/Profile.js
+++ client/src/components/UserManagment/Profile.js
@@ -9,14 +9,16 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import PropTypes from 'prop-types';
 import axios from 'axios'
+import {Button} from "@mui/material";
+import ProfileForm from "./ProfileForm";
 
 // import profileLogo from'../../../public/images/emptyProfileIcon.jpeg';
 
 const Profile = () => {
     // name, location_x, location_y, cost_per_hour, capacity, wifi, disabled_access, smoke_friendly, description, space_type_id
     const [{ user }] = useContext(AppContext);
-    const [workspaces, setWorkspaces] = useState([])
     const [date, setDate] = useState(new Date)
+    const [workspaces, setWorkspaces] = useState([])
 
     const getWorkspaceAvailabilities = async () => {
         await axios.get(`http://localhost:8000/availabilities/${user.id}?date=${date}`, {
@@ -32,79 +34,24 @@ const Profile = () => {
     }
 
     const getHostWorkspaces = async () => {
-            await axios.get(`http://localhost:8000/workspaces/hosts/${user.id}`, {
-            })
-            .then((res) => {
-                console.log("success")
-                setWorkspaces(res.data)
-            })
-            .catch(err =>{
-                alert(err.response.data)
-                setWorkspaces({})
-            })
+        const { data } = await axios.get(`http://localhost:8000/workspaces/hosts/${user.id}`, {});
     }
 
-    useEffect(() => {
-        getHostWorkspaces()
+    useEffect(async () => {
+        await getHostWorkspaces()
         // getWorkspaceAvailabilities()
     }, [])
-    
+
+
 
     return (
-        <div>
-            <div className="profile-data">
-                Profile
-                <img src={user.profileIcon || "emptyProfileIcon.jpeg"}/>
-                <a>name: {user.username}</a>
-                <br/>
-                <a>email: {user.email}</a>
-                <br/>
-                <a>password: {user.password}</a>
+        <div className={'flex h-full'}>
+            <div className={'flex w-1/3'}>
+                <ProfileForm />
             </div>
-            {user.is_host && 
-            <div className="host-places">
-                <a>My Workspaces</a>
-                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                    {workspaces && workspaces.map(workspace => 
-                        <ListItem alignItems="flex-start">
-                        <ListItemAvatar>
-                            <Avatar alt={workspace.name} src={workspace.icon || "/emptyProfileIcon.jpeg"} />
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary={workspace.name}
-                            secondary={
-                                <React.Fragment>
-                                    {workspace.description}
-                                </React.Fragment>
-                            }
-                        />
-                    </ListItem>
-                    )}
-                </List>
+            <div className={'flex-1'}>
+                111
             </div>
-            }
-            {!user.is_host && 
-            <div className="host-places">
-                <a>My Rented Workspaces</a>
-                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                    {workspaces && workspaces.map(workspace => 
-                        <ListItem alignItems="flex-start">
-                        <ListItemAvatar>
-                            <Avatar alt={workspace.name} src={workspace.icon || "/emptyProfileIcon.jpeg"} />
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary={workspace.name}
-                            secondary={
-                                <React.Fragment>
-                                    {workspace.description}
-                                </React.Fragment>
-                            }
-                        />
-                    </ListItem>
-                    )}
-                </List>
-            </div>
-            }
         </div>
     )
 }
