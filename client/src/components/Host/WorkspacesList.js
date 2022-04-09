64b49c47267e96a10e7c38d86670c4dc8bb7a0d5
Yuvala191 <10yuval11@gmail.com>
2022-04-09 18:54:58 +0300

    Fix click on workspace in my spaces

--- client/src/components/Host/WorkspacesList.js
+++ client/src/components/Host/WorkspacesList.js
@@ -1,40 +1,43 @@
-import React, {useContext, useEffect, useState, useCallback} from "react";
+import React, { useContext, useEffect, useState, useCallback } from "react";
 import WorkspacesListItem from "../WorkspaceManagment/WorkspacesListItem";
 import Axios from "axios";
-import {AppContext} from "../../store/AppContext";
+import { AppContext } from "../../store/AppContext";
 
-const WorkspacesList = ({isEditable}) => {
-    const [workspaces, setWorkspaces] = useState([]);
-    const [{user}] = useContext(AppContext);
+const WorkspacesList = ({ isEditable }) => {
+  const [workspaces, setWorkspaces] = useState([]);
+  const [{ user }] = useContext(AppContext);
 
-    const onDelete = (id) => {
-        setWorkspaces(workspaces => {
-            let newArray = workspaces
-            workspaces = newArray.filter(obj => obj.id !== id)
-            return workspaces
-        })
+  const onDelete = (id) => {
+    setWorkspaces((workspaces) => {
+      let newArray = workspaces;
+      workspaces = newArray.filter((obj) => obj.id !== id);
+      return workspaces;
+    });
+  };
+
+  useEffect(async () => {
+    try {
+      const query = {};
+      const res = await Axios.get(`http://localhost:8000/workspaces/hosts/${user.id}`);
+      setWorkspaces(res.data);
+    } catch (err) {
+      console.log(`Failed to fetch workspaces ${err.message}`);
     }
+  }, []);
 
-    useEffect(async () => {
-        try {
-            const query = {};
-            const res = await Axios.get(`http://localhost:8000/workspaces/hosts/${user.id}`);
-            setWorkspaces(res.data);
-        } catch (err) {
-            console.log(`Failed to fetch workspaces ${err.message}`);
-        }
-    }, []);
-
-    return (
-        <div className={'text-white flex flex-col flex-1'}>
-            {
-                workspaces && workspaces.map((workspace) => (
-                    <WorkspacesListItem workspace={workspace} onDelete={onDelete} isEditable={isEditable}/>
-                    )
-                )
-            }
-        </div>
-    );
-}
+  return (
+    <div className={"text-white flex flex-col flex-1"}>
+      {workspaces &&
+        workspaces.map((workspace) => (
+          <WorkspacesListItem
+            workspace={workspace}
+            key={workspace.id}
+            onDelete={onDelete}
+            isEditable={isEditable}
+          />
+        ))}
+    </div>
+  );
+};
 
 export default WorkspacesList;
