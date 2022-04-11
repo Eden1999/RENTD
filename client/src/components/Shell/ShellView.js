c9d1b2b45e9c7d75095b47166457e48b631e2ecf
Yuvala191 <10yuval11@gmail.com>
2022-04-11 22:35:56 +0300

    Fix map in search results view

--- client/src/components/Shell/ShellView.js
+++ client/src/components/Shell/ShellView.js
@@ -1,26 +1,30 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import ProtectedRoute from '../../helpers/routes/ProtectedRoute';
-import routes from '../../helpers/routes/routes';
-import ShellAppBar from './AppBar/ShellAppBar';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import ProtectedRoute from "../../helpers/routes/ProtectedRoute";
+import routes from "../../helpers/routes/routes";
+import ShellAppBar from "./AppBar/ShellAppBar";
 
 function ShellView() {
-    return (
-        <BrowserRouter>
-            <div className="min-h-screen flex flex-col bg-zinc-800">
-                <ShellAppBar />
-                    <Routes>
-                        { routes.map((route) => {
-                            return (
-                                <Route key={route.path} path='' element={<ProtectedRoute allowedUserType={route.allowed}/>}>
-                                    <Route path={route.path} element={route.Component}/>
-                                </Route>
-                            )
-                        })}
-                        <Route path="*" element={<Navigate to="/login" replace />} />
-                    </Routes>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col bg-zinc-800">
+        <ShellAppBar />
+        <Routes>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.path}
+                path=""
+                element={<ProtectedRoute allowedUserType={route.allowed} />}
+              >
+                <Route path={route.path} element={route.Component} />
+              </Route>
+            );
+          })}
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default ShellView;
--- client/src/components/WorkspaceManagment/WorkspacesListItem.js
+++ client/src/components/WorkspaceManagment/WorkspacesListItem.js
@@ -1,21 +1,27 @@
 import React, { useCallback, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import IconButton from '@mui/material/IconButton';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import {Link} from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Link } from "react-router-dom";
 import { AppContext } from "../../store/AppContext";
-import axios from 'axios'
+import axios from "axios";
 import useToken from "../../helpers/useToken";
 
-const WorkspacesListItem = ({ workspace, isEditable, onDelete }) => {
+const WorkspacesListItem = ({
+  workspace,
+  isEditable,
+  onDelete,
+  hoveredOnMap,
+  setHoveredWorkspaceId,
+}) => {
   const navigate = useNavigate();
   const [{ user }] = useContext(AppContext);
   const { token, setToken } = useToken();
-  const [ , dispatch] = useContext(AppContext)
+  const [, dispatch] = useContext(AppContext);
 
   const onItemClick = useCallback((e, workspace) => {
-    navigate(`/workspaces/${workspace.id}`, { state: { workspace, isEditable} });
+    navigate(`/workspaces/${workspace.id}`, { state: { workspace, isEditable } });
   }, []);
 
   const onEditClick = useCallback((e, workspace) => {
@@ -26,48 +32,69 @@ const WorkspacesListItem = ({ workspace, isEditable, onDelete }) => {
   }, []);
 
   const onDeleteClick = useCallback((e, workspace) => {
-    axios.delete(`http://localhost:8000/workspaces/${workspace.id}`, 
-    {headers: {
-      token,
-    }},)
-    .then((res) => {
-      onDelete(workspace.id)
-      navigate('/my-spaces');
-    })
-    .catch(err =>{
-        alert(err.response.data)
-    })
+    axios
+      .delete(`http://localhost:8000/workspaces/${workspace.id}`, {
+        headers: {
+          token,
+        },
+      })
+      .then((res) => {
+        onDelete(workspace.id);
+        navigate("/my-spaces");
+      })
+      .catch((err) => {
+        alert(err.response.data);
+      });
 
     if (!e) var e = window.event;
     e.cancelBubble = true;
     if (e.stopPropagation) e.stopPropagation();
   }, []);
 
-  
+  const onItemHover = useCallback(
+    (workspaceId) => {
+      setHoveredWorkspaceId(workspaceId);
+    },
+    [setHoveredWorkspaceId]
+  );
+
+  const onItemStopHover = useCallback(() => {
+    setHoveredWorkspaceId(null);
+  }, [setHoveredWorkspaceId]);
+
   return (
     <div
       className={`flex bg-zinc-500 hover:bg-zinc-500/90 transition-all duration-200 rounded-lg p-3
-            hover:cursor-pointer mt-5    
+            hover:cursor-pointer mt-5 ${hoveredOnMap ? "hover" : ""}  
         `}
-      //   className={`item ${
-      //     hoveredMarker === workspace.id || hoveredWorkspaceId === workspace.id ? "hover" : ""
-      //   }`}
-      //   onMouseEnter={() => onItemHover(workspace.id)}
-      //   onMouseLeave={onItemStopHover}
+      onMouseEnter={() => onItemHover(workspace.id)}
+      onMouseLeave={onItemStopHover}
       id="listItem"
       onClick={(e) => onItemClick(e, workspace)}
     >
       <img src={workspace.photo} className={"h-28 w-48 bg-zinc-400 rounded-md"} />
       <div className={"flex flex-col text-left flex-1 ml-8"}>
         <span className={"text-xl text-zinc-100"}>{workspace.name}</span>
-        {isEditable && (<span>
-            <IconButton id="editIcon" aria-label="edit workspace" color='primary' onClick={(e) => onEditClick(e, workspace)}>
+        {isEditable && (
+          <span>
+            <IconButton
+              id="editIcon"
+              aria-label="edit workspace"
+              color="primary"
+              onClick={(e) => onEditClick(e, workspace)}
+            >
               <EditIcon />
             </IconButton>
-            <IconButton id="deleteIcon" aria-label="edit workspace" color='primary' onClick={(e) => onDeleteClick(e, workspace)}>
+            <IconButton
+              id="deleteIcon"
+              aria-label="edit workspace"
+              color="primary"
+              onClick={(e) => onDeleteClick(e, workspace)}
+            >
               <DeleteIcon />
             </IconButton>
-        </span>)}
+          </span>
+        )}
         <span className={"text-sm text-zinc-300 mt-1.5"}>
           {workspace.address}, {workspace.city}
         </span>
