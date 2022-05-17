a6232a40733c0ca93c9404f7eb945d04cee873ae
shirelronis <shirelbar100@gmail.com>
2022-05-17 10:13:24 +0300

    remove harry up

--- client/src/components/WorkspaceManagment/WorkspaceDetails.js
+++ client/src/components/WorkspaceManagment/WorkspaceDetails.js
@@ -100,7 +100,7 @@ const WorkspaceDetails = () => {
           <p className="text-sm text-white pr-3">({workspace.ratings.length} reviews)</p>  
         </div>
         {workspace.spaceType && <p className="text-md text-white">{workspace.spaceType.name}</p>}
-        <p className="text-amber-800 pb-7">Hurry up! {workspace.capacity} spots left</p>
+        {/* <p className="text-amber-800 pb-7">Hurry up! {workspace.capacity} spots left</p> */}
         <div className="details">
           <div className="flex flex-column">
           </div>
