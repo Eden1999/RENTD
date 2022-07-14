d901c32a04e1dac95fd09efb652c17b341d97cfe
Eden <314713934.idf.il>
2022-07-14 21:17:15 +0300

    add home page validation

--- client/src/modules/Homepage/WorkspaceForm.js
+++ client/src/modules/Homepage/WorkspaceForm.js
@@ -1,7 +1,7 @@
 import { LocationOn } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import Axios from "axios";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import NumberInput from "../../components/NumberInput";
 
 function WorkspaceForm() {
@@ -9,12 +9,26 @@ function WorkspaceForm() {
   const [city, setCity] = useState({});
   const [capacity, setCapacity] = useState(1);
   const [spaceType, setSpaceType] = useState({});
+  const navigate = useNavigate();
+
+  const handleValidations = () => {
+    let isFormValid = true;
+
+    if (!city?.name || capacity < 1) {
+      isFormValid = false;
+    }
+
+    return isFormValid;
+  };
 
   // Get space types from the server to the environment selector
   useEffect(async () => {
     try {
       const query = {};
-      const res = await Axios.get(`${process.env.REACT_APP_SERVER_URL}spacetypes`, query);
+      const res = await Axios.get(
+        `${process.env.REACT_APP_SERVER_URL}spacetypes`,
+        query
+      );
       setSpaceTypes(res.data);
       setSpaceType(res.data[0]);
     } catch (err) {
@@ -31,7 +45,10 @@ function WorkspaceForm() {
         types: ["locality"],
       };
       setTimeout(() => {
-        const autoComplete = new window.google.maps.places.Autocomplete(locationInput, options);
+        const autoComplete = new window.google.maps.places.Autocomplete(
+          locationInput,
+          options
+        );
         autoComplete.addListener("place_changed", () => {
           const place = autoComplete.getPlace();
           setCity({ name: place.name, location: place.geometry.location });
@@ -45,7 +62,10 @@ function WorkspaceForm() {
   return (
     <form autoComplete="off">
       <div className="mb-6">
-        <label htmlFor="location" className="block mb-2 text-lg font-medium text-primary">
+        <label
+          htmlFor="location"
+          className="block mb-2 text-lg font-medium text-primary"
+        >
           Search for workspace at:
         </label>
         <div className="relative">
@@ -60,12 +80,16 @@ function WorkspaceForm() {
         </div>
       </div>
       <div className="mb-6">
-        <label htmlFor="environment" className="block mb-2 text-lg font-medium text-primary">
+        <label
+          htmlFor="environment"
+          className="block mb-2 text-lg font-medium text-primary"
+        >
           Environment:
         </label>
         <select
           className="select select-bordered 2xl:select-lg font-normal w-full text-secondary"
           onChange={(event) => setSpaceType(spaceTypes[event.target.value])}
+          required
         >
           {spaceTypes.map((item, index) => (
             <option value={index} key={item.id}>
@@ -76,18 +100,32 @@ function WorkspaceForm() {
       </div>
       <div className="flex justify-between items-end mb-6">
         <div className="w-1/2">
-          <label htmlFor="capacity" className="block mb-2 text-lg font-medium text-primary">
+          <label
+            htmlFor="capacity"
+            className="block mb-2 text-lg font-medium text-primary"
+          >
             Number of people:
           </label>
           <NumberInput id="capacity" value={capacity} setValue={setCapacity} />
         </div>
         <div>
-          <Link
-            to={"/search"}
-            state={{ city: JSON.stringify(city), capacity, space_type_id: spaceType.id }}
+          <button
+            type="submit"
+            onClick={(e) => {
+              if (handleValidations()) {
+                navigate("/search", {
+                  state: {
+                    city: JSON.stringify(city),
+                    capacity,
+                    space_type_id: spaceType.id,
+                  },
+                });
+              }
+            }}
+            className="btn btn-lg btn-primary"
           >
-            <button className="btn btn-lg btn-primary">Search</button>
-          </Link>
+            Search
+          </button>
         </div>
       </div>
     </form>
