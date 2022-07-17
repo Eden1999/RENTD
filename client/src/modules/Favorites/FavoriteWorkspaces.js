c4ec8904aa3fc755c6562f920a9a2c604814005a
Eden1999 <37039608+Eden1999@users.noreply.github.com>
2022-07-17 16:27:18 +0300

    Fix/favorites (#114)
    * Fix loading when empty favorites
    
    * Update favorites page when removing favorite
    
    Co-authored-by: Eden <314713934.idf.il>

--- client/src/modules/Favorites/FavoriteWorkspaces.js
+++ client/src/modules/Favorites/FavoriteWorkspaces.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Axios from "axios";
 import { useNavigate } from "react-router-dom";
 import useToken from "helpers/useToken";
 import WorkspacesList from "../../components/WorkspacesList";
 import {workspaceListType} from "../../helpers/consts";
+import { AppContext } from "store/AppContext";
 
 const FavoriteWorkspaces = () => {
   const navigate = useNavigate();
   const [workspaces, setWorkspaces] = useState([]);
   const { token } = useToken();
   let [loading, setLoading] = useState(true);
-
+  const [{user}] = useContext(AppContext);
   useEffect(async () => {
     try {
       const res = await Axios.get(`${process.env.REACT_APP_SERVER_URL}workspaces/userFavoriteWorkspaces`, {
@@ -23,7 +24,7 @@ const FavoriteWorkspaces = () => {
     } catch (err) {
       console.log(`Failed to fetch orders ${err.message}`);
     }
-  }, []);
+  }, [user?.favorite_workspaces]);
 
   const onItemClick = (workspace) => {
     navigate(`/workspaces/${workspace.id}`, { state: { workspace } });
